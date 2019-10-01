fb80a532ab498c784ea72d9211e94b725145b7a3
Giovanny Cerna <gcerna@csumb.edu>
2019-09-30 18:54:32 -0700

    finished twitter

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by user918030 on 9/30/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var profileimageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
